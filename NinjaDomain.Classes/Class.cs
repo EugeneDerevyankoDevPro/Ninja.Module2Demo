88c39fc55158ceab6dd1d65636b2fa5a1c145b3a
Eugene Derevyanko <eugene.derevyanko@dev-pro.net>
2016-02-23 18:39:38 +0200

    inital commit

---
+++ NinjaDomain.Classes/Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NinjaDomain.Classes.Enums;
+
+namespace NinjaDomain.Classes
+{
+	// This project can output the Class library as a NuGet Package.
+	// To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+	public class Ninja
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool ServedInOniwaban { get; set; }
+		public Clan Clan { get; set; }
+		public int ClanId { get; set; }
+		public List<NinjaEquipment> EquipmentOwed { get; set; }
+		public DateTime DateOfBirth { get; set; }
+	}
+
+	public class Clan
+	{
+		public int Id { get; set; }
+		public string ClasnName { get; set; }
+		public List<Ninja> Ninjas { get; set; }
+	}
+
+	public class NinjaEquipment
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public EquipmentType Type { get; set; }
+		[Required]
+		public Ninja Ninja { get; set; }
+	}
+
+}
